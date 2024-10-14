491b9ee79b12f2f5bc460d303f30ce31ebc2ad3a
MaxSalut <m.bezverkhyi28@gmail.com>
2024-10-14 12:34:56 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Веддiть своє число");
+if (int.TryParse(Console.ReadLine(), out int number))
+{
+     Console.WriteLine($"Ви ввели число {number}");
+}
+else
+{
+    Console.WriteLine("Wrong Data");
+}
